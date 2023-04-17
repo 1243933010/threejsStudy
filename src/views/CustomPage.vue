6a7367188ca364afe45076c8593502c2f91a6215
ysys <1243933010@qq.com>
2023-04-17 16:56:31 +0800

    feat(page):加载自己创建的obj模型，添加个3d背景

--- src/views/CustomPage.vue
+++ src/views/CustomPage.vue
@@ -19,5 +19,39 @@ onMounted(() => {
 
     const scene = new THREE.Scene();
     const controls = new OrbitControls(camera,renderer.domElement)
+
+    const loaders = new THREE.CubeTextureLoader();
+    const texture = loaders.load([
+    '/pos-x.jpg','/neg-x.jpg', '/pos-y.jpg','/neg-y.jpg',
+    '/pos-z.jpg', '/neg-z.jpg',
+    ])
+    scene.background=  texture;
+
+    const loader = new GLTFLoader();
+    // loader.setPath('skull_salazar_downloadable/');
+    loader.load('111.glb',(gltf)=>{
+        console.log('11111',gltf)
+        gltf.scene.traverse( function ( child ) {
+    if ( child.isMesh ) {
+        child.frustumCulled = false;
+        //模型阴影
+        child.castShadow = true;
+        
+        //模型自发光
+        child.material.emissive =  child.material.color;
+        child.material.emissiveMap = child.material.map ;
+    }})
+        gltf.scene.scale.set(0.5, 1, 0.5);
+        scene.add(gltf.scene)
+    },undefined,(error)=>{
+        console.log('22222')
+        console.error(error,'---')
+    })
+    function animate() {
+        requestAnimationFrame(animate);
+        controls.update();
+        renderer.render(scene, camera);
+    }
+    animate()
 })
 </script>
